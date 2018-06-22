50847227927fcd50cf35351c2f434e302cb286e0
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-22 20:02:50 +0200

    component for the first view created and a first scatch of login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FirstViewComponent } from './first-view/first-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FirstViewComponent
   ],
   imports: [
     BrowserModule
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,10 +10,19 @@ export class LoginComponent implements OnInit {
   username = 'username of user';
   password = 'password of user';
 
+  makeupColors = ['red', 'blue', 'white', 'black']
+
   constructor() { }
 
   ngOnInit() {
     // Good place to put initialization code
   }
+
+  onClickMe(){
+    // this.username = username;
+    console.log("On click is clicked!")
+  }
+
+  // onKey(event: any){}
 
 }
