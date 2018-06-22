78ab475b9cd548b712384b917b8fbf5100266b87
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-22 20:14:14 +0200

    embedded bride image

--- src/app/first-view/first-view.component.ts
+++ src/app/first-view/first-view.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstViewComponent implements OnInit {
 
-  constructor() { }
+  brideImagePath : string;
+
+  constructor() {
+    this.brideImagePath = '/assets/bride.png'
+  }
 
   ngOnInit() {
   }
